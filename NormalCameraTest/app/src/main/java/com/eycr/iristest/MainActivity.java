bcaa31edd4cbbb8d16af4e8ae812395863a6e4c2
Eduardo Yair Cuellar Rivera <eycr.nm@gmail.com>
2020-12-03 08:29:58 -0600

    Se agregan permisos base para acceso de camaras

--- NormalCameraTest/app/src/main/java/com/eycr/iristest/MainActivity.java
+++ NormalCameraTest/app/src/main/java/com/eycr/iristest/MainActivity.java
@@ -2,13 +2,35 @@ package com.eycr.iristest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.hardware.camera2.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView caracteristicas = (TextView) findViewById(R.id.caracteristicas);
+    String loquetiene = "";
+    private static Context currentcontext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.currentcontext = getApplicationContext();
+        checkCameraHardware(currentcontext);
+    }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            loquetiene.concat(" Tiene camara");
+            return true;
+        } else {
+            // no camera on this device
+            loquetiene.concat(" NO tiene camara");
+            return false;
+        }
     }
 }
